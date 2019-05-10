f7d4487a27a3ce7e6e99deb71490458847cc1d4f
Roderick Diaz <about@rorik.me>
2019-05-10 09:55:57 +0200

    Added about page

--- src/app/docs/docs.module.ts
+++ src/app/docs/docs.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 import { DocsRoutingModule } from './docs-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +10,9 @@ import { AboutComponent } from './about/about.component';
   declarations: [HomeComponent, AboutComponent],
   imports: [
     CommonModule,
-    DocsRoutingModule
+    DocsRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class DocsModule { }
